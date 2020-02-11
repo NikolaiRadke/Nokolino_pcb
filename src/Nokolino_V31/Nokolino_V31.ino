3c7b8efab41ae785da78803e579e6aa6c86ded21
NikolaiRadke <kontakt@nikolairadke.de>
2020-02-11 12:27:40 +0100

    Updated firmware V3.1-110220

--- src/Nokolino_V31/Nokolino_V31.ino
+++ src/Nokolino_V31/Nokolino_V31.ino
@@ -3,7 +3,7 @@
  *  Sketch for Mini-Noko-Monster with new JQ8400 module
  *  For ATtiny45/85 - set to 8 Mhz and remember to flash your bootloader first
  *  
- *  Flash-Usage: 3.986 (IDE 1.8.10 | AVR 1.8.2 | ATtiny 1.0.2 | Linux X86_64 | ATtiny85 )
+ *  Flash-Usage: 3.988 (IDE 1.8.10 | AVR 1.8.2 | ATtiny 1.0.2 | Linux X86_64 | ATtiny85 )
  *  
  *  Circuit:
  *  1: RST | PB5  free
